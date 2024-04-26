eb849ae4947424c6b89edf6b5b8759928fa22fb6
unknown <mluan101999@gmail.com>
2024-04-26 13:43:47 +0700

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Button from '@mui/material/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping, faEnvelope, faMagnifyingGlass, faUser } from '@fortawesome/free-solid-svg-icons'
+
 import './App.css'
+import { Alert } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <div className="header1">
+
+          {/* Logo and seach */}
+          <div>
+            <h1>Shopify</h1>
+            <div className='input'>
+              <input placeholder='Search for a product ...'></input>
+              <FontAwesomeIcon icon={faMagnifyingGlass} style={{ marginLeft: "5px", color: 'white' }} />
+            </div>
+          </div>
+          {/* Logo and seach */}
+
+          {/* Icon */}
+          <div className='user'>
+              <h4>Product</h4>
+              <FontAwesomeIcon icon={faUser} style={{ marginLeft: "20px", marginRight:"5px", color: 'gray' }}  />
+              <h4>Login</h4>
+              <FontAwesomeIcon icon={faCartShopping} style={{ marginLeft: "20px", marginRight:"5px", color: 'gray' }} />
+          </div>
+          {/* Icon */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
